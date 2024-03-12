9364af44ebe4bb6fc4c0ba9e3caaa2e2ed6f98cd
Rachael Brady <rachael.e.brady@gmail.com>
2024-03-12 13:06:28 -0400

    another module error

--- send.py
+++ send.py
@@ -1,5 +1,5 @@
-import bitsv, os
 from bitsv import Key
+import os
 
 client_address = os.environ.get('CLIENT_ADDRESS')# insert the wallet address you're sending money to. client id by export CLIENT_ADDRESS local file
 client_private_key = os.environ.get('CLIENT_PRIVATE_KEY')# insert your wallet's private key by using export CLIENT_PRIVATE_KEY local file
