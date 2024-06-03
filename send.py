812f7dcc0513cc7b4fe16a66ffd9b8bc57baa01a
Rachael Brady <rachael.e.brady@gmail.com>
2024-06-03 16:55:13 -0400

    forgot to switch to main network

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ import os
 client_address = os.environ.get('CLIENT_ADDRESS')# insert the wallet address you're sending money to. client id by export CLIENT_ADDRESS local file
 client_private_key = os.environ.get('CLIENT_PRIVATE_KEY')# insert your wallet's private key by using export CLIENT_PRIVATE_KEY local file
 
-network = 'test' #'test' for Testnet, 'stn' for scaling testnet. 'main' for mainnet.
+network = 'main' #'test' for Testnet, 'stn' for scaling testnet. 'main' for mainnet.
 
 my_key_test = Key(client_private_key, network=network) #private key
 my_key_test.get_balance()
